a9b958ca1d4100e2958f2e5cdb6ce5a81789802f
Shrilakshmi P Naik <naikshrilakshmi@gmail.com>
2024-10-23 20:34:01 +0530

    Add files via upload

---
+++ js4.js
@@ -0,0 +1,34 @@
+const apiKey = 'bd5e378503939ddaee76f12ad7a97608';
+// const apiKey = 'YOUR_API_KEY';
+const locationInput = document.getElementById('location');
+const getWeatherButton = document.getElementById('get-weather');
+const weatherDataDiv = document.getElementById('weather-data');
+
+getWeatherButton.addEventListener('click', getWeather);
+
+function getWeather() {
+    const location = locationInput.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('City not found!!');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const weatherData = `
+                <h2>Current Weather in ${data.name}</h2>
+                <p>Temperature: ${data.main.temp}°C</p>
+                <p>Weather Conditions: ${data.weather[0].description}</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+                <p>Wind Speed: ${data.wind.speed} m/s</p>
+            `;
+            weatherDataDiv.innerHTML = weatherData;
+        })
+        .catch(error => {
+            weatherDataDiv.innerHTML = `<p style="color:blue;">${error.message}</p>`;
+            console.error('Error:', error);
+        });
+}
